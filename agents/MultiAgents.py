c269435ff8e183be42101019bf9d03cc44532b43
Yonish Tayal <yonish@bu.edu>
2025-07-01 14:36:00 -0400

    add single agent and task for industry research

--- agents/MultiAgents.py
+++ agents/MultiAgents.py
@@ -14,3 +14,34 @@ from crewai_tools import ScrapeWebsiteTool, SerperDevTool
 search_tool = SerperDevTool()
 scrape_tool = ScrapeWebsiteTool()
 
+industry = 'Artificial Intelligence'
+
+data_analyst_agent = Agent(
+    role="Market Analyst Agent",
+    goal= "Monitor and analyze market data in real-time "
+         "to identify market trends and competitors in the client's startup's industry",
+    backstory="Specializing in financial markets and venture capital, this agent "
+              "uses statistical modeling and machine learning "
+              "to provide crucial insights. With a knack for data, "
+              "Agent is the cornerstone for "
+              "informing Venture capital Funds about latest innovative startups.",
+    verbose=True,
+    allow_delegation=False,
+    tools = [scrape_tool, search_tool]
+)
+
+data_analysis_task = Task(
+
+    description=(
+    "Continuously monitor and analyze market data for "
+    "the selected startup industry ({{ industry }}). "
+    "Use statistical modeling and machine learning to identify trends and competitors."
+    )
+    ),
+expected_output=(
+        "Insights and alerts about significant market "
+        f"opportunities or threats for making a startup in {industry} industry."
+    ),
+agent=data_analyst_agent,
+)
+
